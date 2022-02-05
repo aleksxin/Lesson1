0d7187d9d63eb0d806195c49f8b23ac9e648711c
Aleksandar Indzhov <aindzhov@allpa.net>
2022-02-05 02:46:41 +0200

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+import ShopPage from './pages/shop/shop.component';
+
+
 
 function App() {
   return (
     <div>
-
-    
-    <HomePage></HomePage>
-
+      <Routes>
+      <Route exact path='/' element={<HomePage/>}/>
+      <Route path='/shop' element={<ShopPage/>}/>
+      </Routes>
     </div>
   );
 }
