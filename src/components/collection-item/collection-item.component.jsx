5c88d7512d567aaec938e685940cf2c4c0df217b
Aleksandar Indzhov <aindzhov@allpa.net>
2022-02-07 00:11:22 +0200

    items show up in the cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -21,7 +21,7 @@ const CollectionItem= ({item, addItem}) => {
             <span className='name'>{name}</span>
             <span className="price">{price}</span>
         </div>
-        <CustomButton  inverted2>ADD TO cart</CustomButton>
+        <CustomButton onClick={() => addItem(item)} inverted2>ADD TO cart</CustomButton>
     </div>
     )
     }
